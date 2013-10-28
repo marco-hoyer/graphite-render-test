73132a49e49ec38d7cc3e5988a3908235226732e
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2013-10-28 17:07:30 +0100

    modified to be able to test for random targets

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ graphite-render-test
 ====================
 
 Some tools for automated testing of the graphite render API
+
+# load testing with random data
+# take 20 random targets
+# request them for 10 times each
+# wait 0.1 s between each iteration
+python graphite-render-test.py <host> --targetfile graphite_index.txt --checknones --interval 0.1 --count 10 --numberoftargets 20
--- src/main/python/graphite-render-test.py
+++ src/main/python/graphite-render-test.py
@@ -11,7 +11,8 @@ import types
 import time
 import datetime
 import argparse
-
+import sys
+import random
 
 def http_get(url, timeout):
     curl = pycurl.Curl()
@@ -49,7 +50,6 @@ def get_graphite_datapoints(url):
     try:
         data = data[0]
     except IndexError:
-        print response
         return None
     return data["datapoints"]
 
@@ -62,44 +62,88 @@ def count_nones(metrics):
                 nones = nones + 1
         return nones
     else:
-        return "No data"
+        return None
 
+def read_file(path):
+    try:
+        return [line.strip() for line in open(path)]
+    except Exception as e:
+        print "Error reading file: " + str(e)
+        sys.exit(2)
 
-# checks metric with an interval of x seconds
-def check_metric_in_interval(url, interval, number_of_checks):
+def get_random_entries(dataset, number_of_entries):
+    random_data = []
+    for count in range(0, number_of_entries):
+        index = random.randint(1,len(dataset))
+        random_data.append(dataset[index])
+    return random_data
+
+def check_single_metric(host, timerange, local, cache, target, interval, number_of_checks):
+    generate_load(host, timerange, local, cache, [target], interval, number_of_checks)
+
+def check_random_metrics(host, timerange, local, cache, targetfile, interval, number_of_checks, number_of_metrics):
+    targets = read_file(targetfile)
+    random_targets = get_random_entries(targets, number_of_metrics)
+    generate_load(host, timerange, local, cache, random_targets, interval, number_of_checks)
+    
+def generate_load(host, timerange, local, cache, targets, interval, number_of_checks):
     max_nones = 0
+    no_data_count = 0
+    
     for count in range(0, number_of_checks):
-        datapoints = get_graphite_datapoints(url)
-
-        nones = count_nones(datapoints)
-        if max_nones < nones:
-            max_nones = nones
-        print str(datetime.datetime.now()) + " - Nones: " + str(nones) + " (" + str(datapoints) + ")"
+        for target in targets:
+            url = build_graphite_render_url(host, target,"json",timerange, local, cache)
+            if args.verbose:
+                print "URL: " + url
+            datapoints = get_graphite_datapoints(url)
+            nones = count_nones(datapoints)
+            if not nones:
+                no_data_count = no_data_count + 1
+            if max_nones < nones:
+                max_nones = nones
+            print str(datetime.datetime.now()) + " - Nones: " + str(nones) + " - Target: " + target
+            if args.verbose:
+                print "Datapoints: " + str(datapoints)
+        print "## waiting " + str(interval) + "s ##"
         time.sleep(interval)
+        
     print "Maximum number of nones was: " + str(max_nones)
-
+    print "Received <No Data> for " + str(no_data_count) + " out of " + str(len(targets)*number_of_checks) + " render requests"
 
 def main(args):
-    timerange = "from=-" + str(args.timerange) + "min"
-    url = build_graphite_render_url(args.host,args.target,"json",timerange, args.local, args.cache)
-    print url
-    if args.checknones:
-        check_metric_in_interval(url, args.interval, args.count)
-    else:
-        print "Usage:"
-        print "--checknones: checks given metric for none values"
-
+    try:
+        timerange = "from=-" + str(args.timerange) + "min"
+    
+        # run checknones with sigle target supplied
+        if args.checknones and args.target:
+            check_single_metric(args.host, timerange, args.local, args.cache, args.target, args.interval, args.count)
+        if args.checknones and args.targetfile and args.numberoftargets:
+            check_random_metrics(args.host, timerange, args.local, args.cache, args.targetfile, args.interval, args.count, args.numberoftargets)
+        else:
+            print "Invalid input supplied - USAGE:"
+            print ""
+            print "Input-Data:"
+            print "  --target <target name> for single target checks"
+            print "  --targetfile <path> to supply a file with targets"
+            print ""
+            print "Actions:"
+            print "  --checknones: checks given metric for none values"
+    except KeyboardInterrupt:
+        sys.exit(0)
 
 if __name__ == '__main__':
     # parameter handling
     parser = argparse.ArgumentParser(description='Instruments backup and replication of applications configured in a yaml config file')
     parser.add_argument("host", help="Graphite Host", type=str)
-    parser.add_argument("target", help="Graphite metric target", type=str)
+    parser.add_argument("--target", help="Graphite metric target", type=str)
+    parser.add_argument("--targetfile", help="Graphite metric targets supplied as line-separated list in file", type=str)
+    parser.add_argument("--numberoftargets", help="Number of targets to take randomly from targetfile", type=int)
     parser.add_argument("--local", help="Query only for local metrics on graphite host", action="store_true", default=False)
     parser.add_argument("--cache", help="Query without using cache", action="store_true", default=False)
     parser.add_argument("--timerange", help="Graphite timerange to look at", type=int, default=5)
-    parser.add_argument("--interval", help="Interval for looping requests", type=int, default=1)
+    parser.add_argument("--interval", help="Interval for looping requests", type=float, default=1)
     parser.add_argument("--count", help="Number of requests to make", type=int, default=60)
     parser.add_argument("--checknones", help="Check number and maximum of nones for a metric", action="store_true")
+    parser.add_argument("--verbose", help="Display verbose output like requested url", action="store_true", default=False)
     args = parser.parse_args()
     main(args)
